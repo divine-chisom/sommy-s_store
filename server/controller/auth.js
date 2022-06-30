9c81b64237d270719338de4f1684d8f83ddefca7
divine-chisom <divinechisom1995@gmail.com>
2022-06-30 17:37:09 +0100

    userlogin

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -100,4 +100,43 @@ class Auth {
     }
   }
 
- 
+  /* User Login/Signin controller  */
+  async postSignin(req, res) {
+    let { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({
+        error: "Fields must not be empty",
+      });
+    }
+    try {
+      const data = await userModel.findOne({ email: email });
+      if (!data) {
+        return res.json({
+          error: "Invalid email or password",
+        });
+      } else {
+        const login = await bcrypt.compare(password, data.password);
+        if (login) {
+          const token = jwt.sign(
+            { _id: data._id, role: data.userRole },
+            JWT_SECRET
+          );
+          const encode = jwt.verify(token, JWT_SECRET);
+          return res.json({
+            token: token,
+            user: encode,
+          });
+        } else {
+          return res.json({
+            error: "Invalid email or password",
+          });
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+const authController = new Auth();
+module.exports = authController;
